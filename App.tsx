7c00b9a8a1cda60d43142a2d3cc7259d88919c4a
Gavrilikhin Daniil <gavrilikhin.daniil@gmail.com>
2025-10-30 16:07:35 +0300

    Skeleton

--- App.tsx
+++ App.tsx
@@ -5,12 +5,13 @@
  * @format
  */
 
-import { NewAppScreen } from '@react-native/new-app-screen';
-import { StatusBar, StyleSheet, useColorScheme, View } from 'react-native';
+import { RTCView, mediaDevices, MediaStream } from "react-native-webrtc"
+import { Button, StatusBar, StyleSheet, useColorScheme, View } from 'react-native';
 import {
   SafeAreaProvider,
   useSafeAreaInsets,
 } from 'react-native-safe-area-context';
+import { useEffect, useState } from "react";
 
 function App() {
   const isDarkMode = useColorScheme() === 'dark';
@@ -24,14 +25,22 @@ function App() {
 }
 
 function AppContent() {
-  const safeAreaInsets = useSafeAreaInsets();
+  const [stream, setStream] = useState<MediaStream>()
+  useEffect(() => {
+    mediaDevices.getUserMedia({ video: true }).then(s => setStream(s))
+  }, []) 
+
+  const insets = useSafeAreaInsets()
+
+  const [maxWidth, setMaxWidth] = useState(false)
 
   return (
-    <View style={styles.container}>
-      <NewAppScreen
-        templateFileName="App.tsx"
-        safeAreaInsets={safeAreaInsets}
-      />
+    <View style={[styles.container, { paddingTop: insets.top, paddingBottom: insets.bottom }]}>
+      <Button title="Press me" onPress={() => setMaxWidth(w => !w)} />
+      <View style={{ flexDirection: "row", flex: 1 }}>
+        <RTCView streamURL={stream?.toURL()} style={{ flex: 1 }} />
+        <View style={{ height: "100%", width: maxWidth ? "100%" : "0%" }} />
+      </View>
     </View>
   );
 }
